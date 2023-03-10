6b3fe8bf32600edbaff40fe1e325286c6bc45680
joaoandrenp <joaoandrenobregapinheiro@gmail.com>
2023-03-10 14:39:08 -0300

    dashboard adm

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,24 +1,24 @@
-@extends('layouts.app')
+@extends('admin.layoutsAdmin.appAdmin')
 
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Dashboard') }}</div>
+{{--    <div class="container">--}}
+{{--        <div class="row justify-content-center">--}}
+{{--            <div class="col-md-8">--}}
+{{--                <div class="card">--}}
+{{--                    <div class="card-header">{{ __('Dashboard') }}</div>--}}
 
-                    <div class="card-body">
-                        <h1>Admin</h1>
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
+{{--                    <div class="card-body">--}}
+{{--                        <h1>Admin</h1>--}}
+{{--                        @if (session('status'))--}}
+{{--                            <div class="alert alert-success" role="alert">--}}
+{{--                                {{ session('status') }}--}}
+{{--                            </div>--}}
+{{--                        @endif--}}
 
-                        {{ __('You are logged in!') }}
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+{{--                        {{ __('You are logged in!') }}--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </div>--}}
 @endsection
--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,18 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
+                'chart.js/Chart.min.js',
+                'jquery-easing/jquery.easing.min.js',
+                'bootstrap/js/bootstrap.bundle.min.js',
+                'jquery/jquery.min.js',
+                'fontawesome-free/css/all.min.css',
+                'resources/css/sb-admin-2.css',
+                'resources/css/styles.css',
                 'resources/js/app.js',
+                'resources/js/sb-admin-2.js',
+                'resources/js/demo/chart-area-demo.js',
+                'resources/js/demo/chart-pie-demo.js',
+                'resources/js/scripts.js',
             ],
         }),
     ],
